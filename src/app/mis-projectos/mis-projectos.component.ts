4c6bce6b826c5a038038869f46f293081f80510a
Tencks <robertinospini@hotmail.com>
2022-08-08 15:10:55 -0300

    git commit -m "cambios varios"

--- src/app/mis-projectos/mis-projectos.component.ts
+++ src/app/mis-projectos/mis-projectos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mis-projectos',
   templateUrl: './mis-projectos.component.html',
-  styleUrls: ['./mis-projectos.component.css','./mis-projectos.component.scss']
+  styleUrls: ['./mis-projectos.component.css']
 })
 export class MisProjectosComponent implements OnInit {
 
